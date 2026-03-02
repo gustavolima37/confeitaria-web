28551f39c7e6f69733c5c021ad6a5bce86fa8302
Seu Nome <seu-email@example.com>
2026-03-02 10:08:48 -0300

    Adiciona migrations do  app pedidos

---
+++ pedidos/views.py
@@ -0,0 +1,30 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .models import Pedido
+# Create your views here.
+
+class PedidoListView(ListView):
+    model = Pedido
+    template_name = 'pedidos/lista.html'
+
+class PedidoDetailView(DetailView):
+    model = Pedido
+    template_name = 'pedidos/detalhe.html'
+       
+class PedidoCreateView(CreateView):
+    model = Pedido
+    fields = ['cliente', 'cpf_cliente', 'produto', 'quantidade','valor_encomenda','forma_pagamento','data_pedido','data_entrega']
+    template_name = 'pedidos/form.html'
+    success_url = reverse_lazy("lista_pedidos")
+
+class PedidoUpdateView(UpdateView):
+    model = Pedido
+    template_name = 'pedidos/form.html'
+    fields = ['cliente', 'cpf_cliente', 'produto', 'quantidade','valor_encomenda','forma_pagamento','data_pedido','data_entrega']
+    success_url = reverse_lazy("lista_pedidos")
+
+class PedidoDeleteView(DeleteView):
+    model = Pedido
+    template_name = 'pedidos/confirm_delete.html'
+    success_url = reverse_lazy("lista_pedidos")
